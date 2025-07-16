9391a62e8b9efc74a4ddc0f45424123dbd382b6e
Matthias Wurm <wurm.matthias@gmx.net>
2025-07-16 15:33:12 +0200

    loginmodal component

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { Loginmodal } from '../loginmodal/loginmodal';
+import { Button } from '../button/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, NgIf],
+  imports: [RouterModule, Loginmodal, Button, NgIf],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   isMobileMenuOpen = false;
+  showLogin = false;
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  toggleLoginModal() {
+    this.showLogin = !this.showLogin;
   }
 }
